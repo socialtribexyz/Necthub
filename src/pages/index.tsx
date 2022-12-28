d1f3a213d1108872e0055f1a90c2c7aa63879d51
sscodersen <marketing.businessblueprint@gmail.com>
2022-12-28 15:06:41 -0800

    linkupdate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -118,7 +118,7 @@ const Home = () => {
 						Generate Widget
 					</Button>
 				</div>
-				<img src="https://imgur.com/JhoCg0A" className="rounded-lg" />
+				<img src="https://i.imgur.com/JhoCg0A.jpg" className="rounded-lg" />
 			</section>
 		</>
 	);
